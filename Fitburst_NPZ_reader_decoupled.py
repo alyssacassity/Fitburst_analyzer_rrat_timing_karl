f105ae68b0eab77fa7710d1614678fadf454be7f
ktsang45 <ktsang45@student.ubc.ca>
2024-05-31 14:09:41 -0700

    Testing

--- Fitburst_NPZ_reader_decoupled.py
+++ Fitburst_NPZ_reader_decoupled.py
@@ -38,4 +38,4 @@ for i in range(len(npz_files)):
     print(npz_files[i])
 
     #os.system('python fitburst_pipeline.py ' + r'\\wsl.localhost\Ubuntu\home\ktsang45\NPZ_files' + r'\\'+ file + ' --outfile')
-    os.system('python fitburst_pipeline.py '  +' --outfile '+ npz_files[i] )
+    os.system('python ~/scratch/Python_code/fitburst_pipeline.py '  +' --outfile '+ npz_files[i] )
