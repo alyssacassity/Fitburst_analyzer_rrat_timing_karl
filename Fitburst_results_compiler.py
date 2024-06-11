778ad04fbcad2942a35b62abb780decf9486790b
ktsang45 <ktsang45@student.ubc.ca>
2024-06-11 13:01:58 -0700

    Testing

--- Fitburst_results_compiler.py
+++ Fitburst_results_compiler.py
@@ -63,7 +63,7 @@ filtime = np.array(filtime)/86400
 """Some code here for calling  fitburst_pipeline.py on the .npz files and 
     iterate over them."""
 for i in range(len(tstart_list)):    
-    toa_list.append(tstart_list[i]+filtime[i]+results_toa[i])
+    toa_list.append(float(tstart_list[i])+float(filtime[i])+float(results_toa[i]))
 print("TOA_list", toa_list)
 print(len(toa_list))
 
