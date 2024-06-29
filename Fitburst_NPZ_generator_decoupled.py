4c12c3c52423911bbbb26294457bfd372bd1e7a7
adam <adamdong@phas.ubc.ca>
2024-06-28 23:47:36 -0700

    debugging Karl's code, seems to work now

--- Fitburst_NPZ_generator_decoupled.py
+++ Fitburst_NPZ_generator_decoupled.py
@@ -23,88 +23,56 @@ def singlecut(fil_name, t_start, disp_measure, fil_time, t_origin, isddp=True):
     # Remove suffix from filename for naming .npz file later
     fil_short_name = [i for i in fil_name.split("/") if sub in i][0].removesuffix(sub)
     fbh = fbfile.header
-
+    downsamp=8 
     # Define variables
-    t_block = 1
+    t_block = 4
     nsamps = int(t_block/fbh.tsamp)
-    downsamp = 12
-    dsampfreq = 8
     nsamps = nsamps-nsamps%downsamp
     #t_start = 396.3
     print(nsamps)
     start_samp = round(t_start/fbh.tsamp)
     #print(start_samp)
+
     fblock = fbfile.read_block(start_samp, nsamps)
     #fbd = fbfile.read_dedisp_block(round(200/fbh.tsamp), 59000, 112.5)
     #print(fblock)
 
     # Remove channels depending on MJD of file
-    if filmjd[ind] in file:
-        if (int(filmjd[ind])>=59091 and int(filmjd[ind])<59093):
-            ignored_chans.extend(list(range(444,470)))
-        elif int(filmjd[ind])>=59093:
-            ignored_chans.extend(list(range(405,470)))
-        elif int(filmjd[ind])>=59523:
-            ignored_chans.extend(list(range(83,108)))
-
-    # Identify bad channels for masking and dedisperse data if needed
-
-    if not isddp:
-        fbt = fblock.dedisperse(dm=disp_measure)
-        disp_measure = 0
-    else:
-        fbt = fblock
-        disp_measure = 0
-
+    if (int(filmjd[ind])>=59091 and int(filmjd[ind])<59093):
+        ignored_chans.extend(list(range(444,470)))
+    elif int(filmjd[ind])>=59093:
+        ignored_chans.extend(list(range(405,470)))
+    elif int(filmjd[ind])>=59523:
+        ignored_chans.extend(list(range(83,108)))
+    ignored_chans.extend(list(range(380,520)))
+    #mask these channels
     #Mask all known RFI channels
+    import copy
+    fbt = copy.deepcopy(fblock)
     mn = np.mean(fbt, axis = 1)
     mask = (mn == 0)
     fbt[mask,:] = np.median(fbt[~mask,:])
     mad_mask = mad(fbt, axis=1) > 0.2
-    print('Mad mask is ' + str(list(mad_mask)))
-    mask[mad_mask] = True
+    #mask[mad_mask] = True
     fbt[mask,:] = np.median(fbt[~mask,:])
     std_on = np.std(fbt, axis=1)
-    print('Mask is ' + str(list(mask)))
     iqrmmask,votes = iqrm_mask(std_on, threshold=1, ignorechans=ignored_chans)
-    print('Iqrmmask is ' + str(list(iqrmmask)))
     mask[iqrmmask] = True
-    fbt_plot = fbt
-    print('ignored channels '+str(ignored_chans))
-
-    # Remove bad channels using mask
-    for i in range(len(mask)):
-        print(len(mask))
-        print('fbt plot' + str(fbt_plot[:,i]))
-        fbt_plot[i,:] = fbt[i,:]
-        #fbt_plot[:,i] = np.mean(fbt, axis=1)
-        if i in ignored_chans:
-            mask[i] = True
-            print('channel masked '+ str(i))
-        print('i is ' + str(i) + ' '+ str(mask[i]))
-        if mask[i]:
-            fbt[i,:] = np.median(fbt[i,:])
-            fbt_plot[i,:] = np.median(fbt_plot[i,:])
-    fbt = fbt.downsample(downsamp, dsampfreq)
+    mask[ignored_chans] = True
+    fbt[mask,:] = np.median(fbt[~mask,:])
+    # Identify bad channels for masking and dedisperse data if needed
+    #import pdb; pdb.set_trace()
+    fbt = fbt.dedisperse(130)
     fbt = fbt.normalise()
-    fbt_plot = fbt_plot.downsample(downsamp, dsampfreq)
-    fbt_plot = fbt_plot.normalise()
-
-
-    # Create metadata and burst parameters dictionaries
-    metadata = dict(bad_chans = 0, freqs_bin0 = fbh.fch1, is_dedispersed = isddp,
-                    num_time = nsamps/downsamp, num_freq = fbh.nchans/dsampfreq,
-                    times_bin0 = fbh.tstart+t_start/86400,  res_time = fbh.tsamp*downsamp, res_freq = fbh.foff*dsampfreq)
-    burst_parameters = dict(ref_freq = [600.2], amplitude = [np.log10(np.max(fbt))], arrival_time = [0.5],
-                            burst_width = [0.02], dm = [disp_measure], dm_index = [-2],
-                            scattering_index = [-4], scattering_timescale = [0.01], spectral_index = [0],
-                            spectral_running = [0])
+    fbt = fbt.downsample(downsamp,8)
 
 
     # Plot data and save
     #plt.imshow(fbt, aspect='auto')
     #Saves figure to .png image
-    plt.imshow(fbt, aspect='auto', extent=[0,nsamps*fbh.tsamp, len(mask), 0])
+    
+    plt.imshow(fbt, aspect='auto',cmap="YlGnBu")
+    plt.show()
     plt.savefig(fil_short_name + '_' + str(fil_time) + '_' + str(t_origin) + '_test.png')
     data_full = fbt
     #print(fil_short_name)
@@ -192,4 +160,4 @@ print('test')
 #for file_run in fils_to_run:
 print('filstorun length ' + str(len(fils_to_run)) + 'filtime length ' + str(len(filtime)) + 'fildm length ' + str(len(fildm)) + 'tstart length ' +str(len(tstart_list)))
 print('Index '+ str(ind))
-singlecut(fils_to_run[ind], float(filtime[ind])-0.5, float(fildm[ind]), float(filtime[ind]), float(tstart_list[ind]))
+singlecut(fils_to_run[ind], float(filtime[ind])-2, float(fildm[ind]), float(filtime[ind]), float(tstart_list[ind]))
