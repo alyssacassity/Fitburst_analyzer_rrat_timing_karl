83c7f4a06a4cfa715842b37a6cce912b2cfa073e
CarolusHaifisch <karltsang.ib@gmail.com>
2024-08-19 14:18:23 -0700

    Cleaned up version

--- Fitburst_NPZ_generator_decoupled.py
+++ Fitburst_NPZ_generator_decoupled.py
@@ -17,6 +17,7 @@ ignored_chans = ([0] + list(range(34,48)) + list(range(113,179)) +
                  list(range(185,211)) + list(range(218,255)) + list(range(554,569)) + list(range(584,597)) +
                  list(range(631,645)) + list(range(677,694)) + list(range(754,763))+list(range(788,792))+list(range(854,861))+list(range(873,876))+[887])
 
+# Cuts a chunk of the .fil file for analysis
 def singlecut(fil_name, t_start, disp_measure, fil_time, t_origin, isddp=False):
     fbfile = FilReader(fil_name)
     sub = '.fil'
@@ -30,13 +31,8 @@ def singlecut(fil_name, t_start, disp_measure, fil_time, t_origin, isddp=False):
     nsamps = int(t_block/fbh.tsamp)
     nsamps = nsamps-nsamps%downsamp
     #t_start = 396.3
-    print(nsamps)
-    start_samp = round(t_start/fbh.tsamp)
-    #print(start_samp)
-
+    start_samp = round(t_start/fbh.tsamp) # starting sample block
     fblock = fbfile.read_block(start_samp, nsamps)
-    #fbd = fbfile.read_dedisp_block(round(200/fbh.tsamp), 59000, 112.5)
-    #print(fblock)
 
     # Remove channels depending on MJD of file
     if (int(filmjd[ind])>=59091 and int(filmjd[ind])<59093):
@@ -53,26 +49,22 @@ def singlecut(fil_name, t_start, disp_measure, fil_time, t_origin, isddp=False):
     mn = np.mean(fbt, axis = 1)
     mask = (mn == 0)
     fbt[mask,:] = np.median(fbt[~mask,:])
-    mad_mask = mad(fbt, axis=1) > 0.2
-    #mask[mad_mask] = True
-    fbt[mask,:] = np.median(fbt[~mask,:])
     std_on = np.std(fbt, axis=1)
     iqrmmask,votes = iqrm_mask(std_on, threshold=1, ignorechans=ignored_chans)
     mask[iqrmmask] = True
     mask[ignored_chans] = True
     fbt[mask,:] = np.median(fbt[~mask,:])
     # Identify bad channels for masking and dedisperse data if needed
-    #import pdb; pdb.set_trace()
     if not isddp:
         fbt = fbt.dedisperse(dm=disp_measure)
-        #fbt = fbt.dedisperse(dm=130)
         disp_measure = 0
     else:
         fbt = fbt
         disp_measure = 0
-
+    # Downsample data by ratio downsamp and downsample frequency data by ratio downfreq
     fbt = fbt.downsample(downsamp,dsampfreq)
     fbt = fbt.normalise()
+    # Further cut data down to 1 second block
     zoom_mid_sample = int(t_block/2/fbh.tsamp/downsamp)
     zoom_window = 1 #second
     zoom_window_samples = int(zoom_window/fbh.tsamp/downsamp)
@@ -81,7 +73,7 @@ def singlecut(fil_name, t_start, disp_measure, fil_time, t_origin, isddp=False):
     fbt_zoom = fbt[:, zoom_start_samp:zoom_end_samp]
     fbt = fbt_zoom
     time_bin0 = fbh.tstart + start_samp*fbh.tsamp/86400 + zoom_start_samp*fbh.tsamp*downsamp/86400
-
+    #Populate metadata and burst_parameters dictionaries for generating .npz file
     metadata = dict(bad_chans = 0, freqs_bin0 = fbh.fch1, is_dedispersed = isddp,
                     num_time = zoom_window_samples, num_freq = fbh.nchans/dsampfreq,
                     times_bin0 = time_bin0,  res_time = fbh.tsamp*downsamp, res_freq = fbh.foff*dsampfreq)
@@ -91,15 +83,12 @@ def singlecut(fil_name, t_start, disp_measure, fil_time, t_origin, isddp=False):
                             spectral_running = [0])
 
     # Plot data and save
-    #plt.imshow(fbt, aspect='auto')
     #Saves figure to .png image
     plt.figure()
     plt.imshow(fbt, aspect='auto',cmap="YlGnBu")
     plt.savefig(fil_short_name + '_' + str(fil_time) + '_' + str(t_origin) + '_test.png')
     plt.close()
     data_full = fbt
-    #print(fil_short_name)
-    #print(r'~/NPZ_files/'+ fil_short_name + '_' + fil_time + '.npz')
     np.savez(
         (fil_short_name + '_' + str(fil_time) + '_' + str(t_origin) + ".npz"), 
         data_full=data_full, 
@@ -108,11 +97,9 @@ def singlecut(fil_name, t_start, disp_measure, fil_time, t_origin, isddp=False):
         )
         
     return fbh.tstart
-# Load files for analysis (Read in names of candidates)
-#files = glob.glob(r'\\wsl.localhost\Ubuntu\home\ktsang45\*.fil')
 
-#for file in files:
-    
+
+# Load files for analysis (Read in names of candidates)
 """Use Argparse to enable command line inputs"""
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -132,55 +119,31 @@ parser.add_argument(
     type=str,
     help='Path to all .fil files to be analyzed')
 
-
 args = parser.parse_args()
 ind = args.index
 pulse_path = args.pulse_path
 fils_path = args.fils_path
-
-print(pulse_path)
-print(fils_path)
-
-"""Decide on where to cut the blocks here, then pass the cutting parameters
-as arguments into Fitburst_singlecut_mod.py to cut each file and generate
-an .npz file for each. """
-    
-#files = os.listdir(r'\\wsl.localhost\Ubuntu\home\ktsang45\positive_bursts_1')
 datafile = pulse_path
 data = np.genfromtxt(datafile, delimiter=",", dtype=str)
 files = data[:,0]
-#print(files)
 tstart_list = []
 filtime = []
 fildm = []
 filmjd = []
 
-print('test1')
+# Find and analyze candidate files for pulsar, appends found .fil files to list of files to run by singlecut function
 for file in files:
-    #print(file)
     file = file[file.find('cand'):]
     filparts = file.split('_')
-    #print(filparts)
     filtime.append(filparts[4])
     fildm.append(filparts[6])
     tstart_list.append(filparts[2])
     filmjd.append(str(int(float(filparts[2]))))
-#filfiles = glob.glob(r'\\wsl.localhost\Ubuntu\home\ktsang45\*.fil')
 filfiles = glob.glob(fils_path + r'/*.fil')
-print('filtime is ' + str(filtime))
-print('start time is ' + str(tstart_list))
-#print('test2')
-
 fils_to_run = []
 for i in range(len(files)):
    for file in filfiles:
        if filmjd[i] in file:
             fils_to_run.append(file)
-print('filstorun length ' + str(len(fils_to_run)))
-
 toa_list = []
-print('test')
-#for file_run in fils_to_run:
-print('filstorun length ' + str(len(fils_to_run)) + 'filtime length ' + str(len(filtime)) + 'fildm length ' + str(len(fildm)) + 'tstart length ' +str(len(tstart_list)))
-print('Index '+ str(ind))
 singlecut(fils_to_run[ind], float(filtime[ind])-5, float(fildm[ind]), float(filtime[ind]), float(tstart_list[ind]))
