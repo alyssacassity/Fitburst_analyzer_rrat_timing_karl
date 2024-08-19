f55147b7bf7f83ca283a4b663c78852e112c488a
CarolusHaifisch <karltsang.ib@gmail.com>
2024-08-19 14:25:35 -0700

    Cleaned up version

--- Fitburst_NPZ_reader_decoupled.py
+++ Fitburst_NPZ_reader_decoupled.py
@@ -7,7 +7,7 @@ Created on Wed May 22 12:37:22 2024
 import os
 import argparse
 
-"""Use Argparse to enable command line inputs"""
+# Use Argparse to enable command line inputs
 parser = argparse.ArgumentParser()
 parser.add_argument(
     'index', 
@@ -19,20 +19,15 @@ parser.add_argument(
     action='store', 
     type=str,
     help='Path to all .npz files to be analyzed')
-print('test')
+
 args = parser.parse_args()
 ind = args.index
 npz_path = args.npz_path
-print(npz_path)
 filtime = []
 tstart_list = []
-#npz_files = os.listdir(r'C:\Users\ktsan\Desktop\Research\NPZ_files')
 npz_files = [i for i in os.listdir(npz_path) if '.npz' in i]
-#for i in range(len(npz_files)):
 filparts = npz_files[ind].split('_')
 filtime.append(filparts[3])
 tstart_list.append(filparts[4])
-print(npz_files[ind])
-
-    #os.system('python fitburst_pipeline.py ' + r'\\wsl.localhost\Ubuntu\home\ktsang45\NPZ_files' + r'\\'+ file + ' --outfile')
+# System call to run the fitburst pipeline fitting on each of the .npz files
 os.system('python '+ npz_path + 'fitburst_pipeline.py '  +' --outfile '+ npz_path + npz_files[ind] + ' ' + npz_path )
