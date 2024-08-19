5e48fffef1094620dbf153351b5eae08bc009448
CarolusHaifisch <karltsang.ib@gmail.com>
2024-08-19 14:29:18 -0700

    Cleaned up version

--- Fitburst_results_compiler.py
+++ Fitburst_results_compiler.py
@@ -9,7 +9,7 @@ import argparse
 import numpy as np
 import json
 
-"""Use Argparse to enable command line inputs"""
+# Use Argparse to enable command line inputs
 parser = argparse.ArgumentParser()
 parser.add_argument(
     'json_path',
@@ -22,7 +22,7 @@ json_path = args.json_path
 
 toa_list = []
 
-""" Some code for reading the TOA from the results json file"""
+# Code for reading the TOA from the results .json file generated from the fitburst fitting
 results_files = [i for i in os.listdir(json_path) if '.json' in i]
 
 results_toa = []
@@ -31,7 +31,6 @@ mjd_errors = []
 filtime = []
 tstart_list = []
 fil_names = []
-print(results_files)
 for i in range(len(results_files)):
     with open(json_path + results_files[i], 'r') as f:
         data = json.load(f)
@@ -46,34 +45,19 @@ for i in range(len(results_files)):
             fil_names.append(results_files[i].removesuffix('_'+ str(results_files[i].split('_')[-2])
                                                            +'_'+str(results_files[i].split('_')[-1].removesuffix('.json'))
                                                            +'.json').removeprefix('results_fitburst_'))
-            
 
-'''with open(results_files[ind], 'r') as f:
-    data = json.load(f)
-    results_toa.append((data['model_parameters']['arrival_time'][0]-0.5)/86400)
-    ref_freqs.append(800)
-    if (isinstance(data['fit_statistics']['bestfit_uncertainties']['arrival_time'][0], float) and 
-    (not np.isnan(data['fit_statistics']['bestfit_uncertainties']['arrival_time'][0]))) :
-        mjd_errors.append(data['fit_statistics']['bestfit_uncertainties']['arrival_time'][0])
-    else:
-        mjd_errors.append(1e-6)'''
-print("Results_TOA", results_toa)
-print(filtime)
+
+
 filtime = [float(i) for i in filtime]
-#filtime = float(filtime[ind])
 filtime = np.array(filtime)/86400
 
     
-"""Some code here for calling  fitburst_pipeline.py on the .npz files and 
-    iterate over them."""
+# Call fitburst_pipeline.py on the .npz files and iterate over them
 for i in range(len(tstart_list)):    
     toa_list.append(float(tstart_list[i])+float(filtime[i])+float(results_toa[i]))
-print("TOA_list", toa_list)
-print(len(toa_list))
-print(len(results_files))
 
 
-""" Save data to .tim file"""
+# Save data to a .tim file
 res_file = open('pulsar_timing_results.tim', 'w')
 txt_list = []
 for i in range(len(toa_list)):
